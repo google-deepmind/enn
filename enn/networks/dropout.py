2ba8ee14d46c69d996091e75b0782719279f7467
Mohammad Asghari <smasghari@google.com>
2021-09-09 12:19:57 -0700

    Fixed the issue with haiku.dropout by generating masks directly
    PiperOrigin-RevId: 395759122
    Change-Id: I06061472514de3be56627893a1d2fe334e1df229

--- enn/networks/dropout.py
+++ enn/networks/dropout.py
@@ -15,13 +15,41 @@
 # limitations under the License.
 # ============================================================================
 """Implementing Dropout as an ENN in JAX."""
-from typing import Optional, Sequence
+from typing import Optional, Sequence, List
 
 from enn import base
 from enn.networks import indexers
 import haiku as hk
 import jax
 import jax.numpy as jnp
+
+
+def cummulative_sum(x):
+  """Returns the cummulative sum of elements of a list."""
+  # Create a copy of list x.
+  output = x[:]
+  for i in range(len(output) - 1):
+    output[i + 1] += output[i]
+  return output
+
+
+def generate_masks(key: base.RngKey, input_size: int,
+                   output_sizes: Sequence[int],
+                   dropout_rate: float) -> List[jnp.ndarray]:
+  """Generates masks for nodes of the network excluding nodes of the last layer."""
+  # Create a list of num nodes per layer, ignoring the output layer.
+  num_nodes_layers = [input_size,] + list(output_sizes[:-1])
+  keep_rate = 1.0 - dropout_rate
+  # Create an array of masks, one mask for each nodes of the network, excluding
+  # nodes of the last layer.
+  masks = jax.random.bernoulli(
+      key, keep_rate, shape=[sum(num_nodes_layers)])
+  masks /= keep_rate
+  # Create a list where each element is an array of masks for one layer.
+  # TODO(author3): Use jnp.cumsum instead of cummulative_sum
+  masks = jnp.split(masks, cummulative_sum(num_nodes_layers))
+
+  return masks
 
 
 class MLPDropoutENN(base.EpistemicNetwork):
@@ -40,15 +68,29 @@ class MLPDropoutENN(base.EpistemicNetwork):
     """MLP with dropout as an ENN."""
 
     def enn_fn(inputs: base.Array,
-               index: base.Index) -> base.Output:
-      x = hk.Flatten()(inputs)
+               z: base.Index) -> base.Output:
 
       assert inputs.ndim == 2
       unused_batch, input_size = inputs.shape
-      num_layers = len(output_sizes)
-      # We split index based on the number layers to generate an rng key for
-      # each dropout layer.
-      indices = jax.random.split(index, num_layers)
+
+      x = hk.Flatten()(inputs)
+
+      # Generate a list of masks, one for each network layer, exclduing the
+      # output layer
+      masks = generate_masks(
+          key=z,
+          input_size=input_size,
+          output_sizes=output_sizes,
+          dropout_rate=dropout_rate)
+
+      # Note that we consider a dropout layer after the input to be
+      # consistent with the paper "Dropout as a Bayesian Approximation:
+      # Representing Model Uncertainty in Deep Learning" (2015),
+      # https://github.com/yaringal/DropoutUncertaintyExps/blob/master/net/net.py
+      if dropout_input:
+        mask = masks[0]
+        x *= mask
+
       for layer_index, output_size in enumerate(output_sizes):
         if layer_index == 0:
           if nonzero_bias:
@@ -57,28 +99,24 @@ class MLPDropoutENN(base.EpistemicNetwork):
           else:
             b_init_0 = b_init
           x = hk.Linear(output_size, w_init=w_init, b_init=b_init_0)(x)
-          # Note that we consider a dropout layer after the input to be
-          # consistent with the paper "Dropout as a Bayesian Approximation:
-          # Representing Model Uncertainty in Deep Learning" (2015),
-          # https://github.com/yaringal/DropoutUncertaintyExps/blob/master/net/net.py
-          if dropout_input:
-            x = hk.dropout(indices[layer_index], dropout_rate, x)
         else:
           x = hk.Linear(output_size, w_init=w_init, b_init=b_init)(x)
-          x = hk.dropout(indices[layer_index], dropout_rate, x)
         if layer_index < len(output_sizes) - 1:
+          mask = masks[layer_index + 1]
+          x *= mask
           x = jax.nn.relu(x)
       return x
-    # Note that although our enn_fn is stochastic because of the dropout layer,
-    # since we pass an index as rng directly, we can still wrap transformed
+
+    # Note that our enn_fn is stochastic because we generate masks in it. But,
+    # since we pass a base.RngKey directly to it, we can still wrap transformed
     # function with hk.without_apply_rng.
     transformed = hk.without_apply_rng(hk.transform(enn_fn))
 
     # We use a simple indexer which is basically an identity map.
     indexer = indexers.PrngIndexer()
 
-    # Apply function for enn_fn requires a rng key. We use the index z in f(x,z)
-    # as the rng key. This apply method hides this step from outside.
+    # Apply function for enn_fn requires a rng key to generate masks. We use
+    # the index z in f(x,z) as the rng key.
     def apply(params: hk.Params,
               x: base.Array,
               z: base.Index) -> base.Output:
