Fixed a bug caused by a left over scaling of parameters.
PiperOrigin-RevId: 396670378
Change-Id: I0cefa192333fc1e2d6fee8691d902ed1a60f1402

enn/losses/vi_losses.py
@@ -175,13 +175,12 @@ def get_diagonal_linear_hypermodel_elbo_fn(
     biases = jnp.concatenate(biases, axis=0)
     weights, _ = jax.tree_flatten(weights)
     weights = jnp.concatenate(weights, axis=0)
-    scales = jnp.log(1 + jnp.exp(weights))
-    chex.assert_equal_shape([scales, biases])
+    chex.assert_equal_shape([weights, biases])
     return 0.5  / num_samples * (
-        jnp.sum(jnp.square(scales))
+        jnp.sum(jnp.square(weights))
         + jnp.sum(jnp.square(biases)) / (sigma_0 ** 2)
         - len(biases)
-        - 2 * jnp.sum(jnp.log(scales))
+        - 2 * jnp.sum(jnp.log(weights))
         )
 
   return single_index.ElboLoss(

enn/losses/vi_losses_test.py
@@ -40,9 +40,8 @@ class LossesTest(parameterized.TestCase):
     nelbo_fn = vi_losses.get_diagonal_linear_hypermodel_elbo_fn(
         log_likelihood_fn, sigma_0=sigma_0, num_samples=1)
 
-    w_scale = np.log(np.exp(sigma) - 1)  # sigma = log(1 + exp(w))
     params = {'layer': {
-        'w': w_scale * np.ones(num_params,),
+        'w': sigma * np.ones(num_params,),
         'b': mu * np.ones(num_params,)}}
     kl = 0.5 * num_params * (sigma**2 + mu**2 / sigma_0 - 1 - 2 * np.log(sigma))
     batch = enn_base.Batch(x=np.zeros((2, 1)), y=np.zeros((2, 1)))