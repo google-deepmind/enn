d6eb0d60f5f5350374ada157abf074cc0081a5aa
Morteza Ibrahimi <mibrahimi@google.com>
2021-09-15 14:31:10 -0700

    Simplify BBB constructor.
    PiperOrigin-RevId: 396924483
    Change-Id: I97a407233ad0e7ee6b2be6e0192ee60bb346fde1

--- enn/networks/bbb.py
+++ enn/networks/bbb.py
@@ -17,16 +17,15 @@
 
 """Implementing Bayes-by-backprop (BBB) in JAX."""
 
-from typing import Callable, Optional, Sequence
+from typing import Sequence
 
 import chex
-from enn import base
 from enn import utils
 from enn.networks import hypermodels
 from enn.networks import indexers
 import haiku as hk
 import jax
-import numpy as np
+import jax.numpy as jnp
 
 
 # TODO(author2): Current implementation will produce a bbb based on a linear
@@ -50,48 +49,23 @@ def make_bbb_enn(base_output_sizes: Sequence[int],
 
   transformed_base = make_transformed_base(base_output_sizes)
 
+  base_params = transformed_base.init(jax.random.PRNGKey(0), dummy_input)
+  num_base_params = sum(jax.tree_leaves(jax.tree_map(jnp.size, base_params)))
+
   # VI loss computed by vi_losses.get_linear_hypermodel_elbo_fn assumes the
   # index to be Gaussian with the same variance as the latent prior variance.
-  def indexer_ctor(index_dim):
-    return indexers.ScaledGaussianIndexer(index_dim, index_scale=sigma_0)
-  enn = DiagonalLinearHypermodel(
-      transformed_base=transformed_base,
-      dummy_input=dummy_input,
-      indexer_ctor=indexer_ctor,
-      return_generated_params=True,
-      scale=False,
+  indexer = indexers.ScaledGaussianIndexer(index_dim=num_base_params,
+                                           index_scale=sigma_0)
+
+  enn = utils.epistemic_network_from_module(
+      enn_ctor=hypermodels.hypermodel_module(
+          transformed_base,
+          dummy_input,
+          hyper_torso=lambda x: x,
+          diagonal_linear_hyper=True,
+          return_generated_params=False,
+          scale=False),
+      indexer=indexer,
   )
 
   return enn
-
-
-class DiagonalLinearHypermodel(base.EpistemicNetwork):
-  """Diagonal linear hypermodel for transformed_base as EpistemicNetwork."""
-
-  def __init__(
-      self,
-      transformed_base: hk.Transformed,
-      dummy_input: base.Array,
-      indexer_ctor: Callable[[int], base.EpistemicIndexer],
-      return_generated_params: bool = False,
-      scale: bool = False,
-      w_init: Optional[hk.initializers.Initializer] = None,
-      b_init: Optional[hk.initializers.Initializer] = None,
-      ):
-
-    base_params = transformed_base.init(jax.random.PRNGKey(0), dummy_input)
-    num_base_params = np.sum(
-        jax.tree_leaves(jax.tree_map(np.size, base_params)))
-    indexer = indexer_ctor(num_base_params)
-
-    enn = utils.epistemic_network_from_module(
-        enn_ctor=hypermodels.hypermodel_module(
-            transformed_base,
-            dummy_input,
-            hyper_torso=lambda x: x,
-            diagonal_linear_hyper=True,
-            return_generated_params=return_generated_params,
-            scale=scale),
-        indexer=indexer,
-    )
-    super().__init__(enn.apply, enn.init, enn.indexer)
