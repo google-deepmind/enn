1722f97f8d846d71c2c890b35fb2811227c48b20
DeepMind <noreply@google.com>
2022-04-20 22:52:23 +0000

    Internal change.
    PiperOrigin-RevId: 443215262
    Change-Id: I2ffdb252f664ce83a04f5967f6ec907ce70db4cb

--- test.sh
+++ test.sh
@@ -23,6 +23,17 @@ set -x
 python3 -m venv enn_testing
 source enn_testing/bin/activate
 
+# This prints the path to the python binary we are using. It should be inside
+# the virtualenv you just created (in this case the path should end in
+# enn_testing/bin/python).
+which python
+
+# This installs and/or updates `pip` and `wheel`. It's good practice to do this
+# before you attempt to install anything else inside your virtualenv. In
+# particular if `wheel` is not installed then Python may fall back on building
+# some packages from source, rather than using a precompiled version.
+python -m pip install -U pip wheel
+
 # Install all dependencies.
 pip install .
 
